24f3e5dc913be7c1ff5965622ce01f0c237ecdc2
Dat Tran <datttrian@gmail.com>
2024-04-18 23:22:54 +0000

    Thu, Apr 18, 2024, 11:22 PM +00:00

--- Workspace.sql
+++ Workspace.sql
@@ -11,8 +11,9 @@
 -- a scalar variable for the total purchase amount and initialize this variable to different values to
 -- test your block.
 -- Capture your output and include in a word document.
+SET SERVEROUTPUT ON;
 DECLARE
-    lv_total_purchase  NUMBER := 100;
+    lv_total_purchase  NUMBER := 250;
     lv_customer_rating VARCHAR2(10);
 BEGIN
     IF lv_total_purchase > 200 THEN
